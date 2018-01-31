badf18ec6bd0a8bcb9714b2bf565b4cf77fa28a3
elnochas <elnochas3443@gmail.com>
2018-01-31 13:56:48 +0100

    ejercicio2

--- projects/ejercicio02.php
+++ projects/ejercicio02.php
@@ -8,4 +8,10 @@ echo "<br>";
 echo $_SERVER['HTTP_USER_AGENT'];
 echo "<br>";
 echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";
+echo "<br>";
+foreach ($_SERVER as $key =>$value) {
+    echo "$key : $value";
+    echo "<br>";
+}
 ?>
